c5ef92e7872832b87239ecdb4407e946c73e8c27
hustfc <spfanc@gmail.com>
2019-04-04 10:49:57 +0800

    丢包 重传

--- Stackelberg/net.py
+++ Stackelberg/net.py
@@ -6,17 +6,41 @@ from mn_wifi.cli import CLI_wifi
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.wmediumdConnector import interference
 # from send import send
-# from receive import receive 
+# from receive import receive
 
 
 import time
 import thread
+import threading
 import json
 
+
+
+class MyThread(threading.Thread):
+
+    def __init__(self, func, args=()):
+        super(MyThread, self).__init__()
+        self.func = func
+        self.args = args
+
+    def run(self):
+        self.result = self.func(*self.args)
+
+    def get_result(self):
+        try:
+            return self.result
+        except Exception:
+            return None
+
+
 "function for exec cmd"
+
+
 def command(host, arg):
-    host.cmd(arg)
-    thread.exit()
+    result = host.cmd(arg)
+    return result
+
+
 # "function for create a thread for sending a certern packet "
 # def sendpacket(src,dst,num):
 
@@ -50,7 +74,7 @@ def topology():
     net.configureWifiNodes()
 
     info("*** Adding Link\n")
-    net.addLink(ap1, ap2, cls=_4address) # ap1=ap, ap2=client
+    net.addLink(ap1, ap2, cls=_4address)  # ap1=ap, ap2=client
     net.addLink(h1, ap1)
     net.addLink(h2, ap1)
     net.addLink(h3, ap1)
@@ -58,7 +82,7 @@ def topology():
     # net.addLink(h5, ap1)
     net.addLink(h6, ap1)
     net.addLink(BS, ap1)
-    #net.plotGraph(max_x=100, max_y=100)
+    # net.plotGraph(max_x=100, max_y=100)
 
     info("*** Starting network\n")
     net.build()
@@ -66,77 +90,102 @@ def topology():
     ap1.start([c0])
     ap2.start([c0])
 
-    info("*** first cycle BS collect the info of UE\n")
-    try:
-        thread.start_new_thread(command,(BS,"python RInfo.py 10.0.0.7 BS-eth0"))
-        thread.start_new_thread(command,(h1,"python SInfo.py 10.0.0.1 h1-eth0 10.0.0.7"))
-        thread.start_new_thread(command,(h2,"python SInfo.py 10.0.0.2 h2-eth0 10.0.0.7"))
-        thread.start_new_thread(command,(h3,"python SInfo.py 10.0.0.3 h3-eth0 10.0.0.7"))
-        
-    except:
-        print("first cycle error")
-    time.sleep(22) #wait for collect UE info
-    print("first cycle finish")
+    # info("*** first cycle BS collect the info of UE\n")
+    # try:
+    #     thread.start_new_thread(command,(BS,"python RInfo.py 10.0.0.7 BS-eth0"))
+    #     thread.start_new_thread(command,(h1,"python SInfo.py 10.0.0.1 h1-eth0 10.0.0.7"))
+    #     thread.start_new_thread(command,(h2,"python SInfo.py 10.0.0.2 h2-eth0 10.0.0.7"))
+    #     thread.start_new_thread(command,(h3,"python SInfo.py 10.0.0.3 h3-eth0 10.0.0.7"))
 
-    # the BS start to create the UE's info
-    BSLog={
-        "h1":{},
-        "h2":{},
-        "h3":{}
-        # "h4":{},
-        # "h5":{},
-        # "h6":{}
-    }
-    BSLog["h1"]["flag"]=False
-    BSLog["h2"]["flag"]=False
-    BSLog["h3"]["flag"]=False
-    #filename1 = "/home/shlled/mininet-wifi/Log/BSLog.json"
-    filename1 = '/media/psf/Home/Documents/GitHub/mininet-project/Stackelberg/Log/BSLog.json'
+    # except:
+    #     print("first cycle error")
+    # time.sleep(22) #wait for collect UE info
+    # print("first cycle finish")
 
-    with open(filename1,'r') as f1:
-        buffer=f1.readlines()
-        lenth=len(buffer)
-        while lenth>0:
-            temp=buffer[lenth-1]
-            print(temp)
-            temp=json.loads(temp)
-            print(temp)
-            #print(temp[0]["UEIP"])
-            if temp[0]["UEIP"] == "10.0.0.1" and BSLog["h1"]["flag"] == False:
-                BSLog["h1"]["flag"] = True
-                BSLog["h1"]["IP"] = temp[0]["UEIP"]
-                BSLog["h1"]["POWER"] = temp[0]["UEPOWER"]
-                BSLog["h1"]["PRICE"] = temp[0]["UEPRICE"]
-                BSLog["h1"]["LOSS"] = temp[0]["UELOSS"]
-                BSLog["h1"]["MAX"] = temp[0]["UEMAX"]
-            elif temp[0]["UEIP"] == "10.0.0.2" and BSLog["h2"]["flag"] == False:
-                BSLog["h2"]["flag"] = True
-                BSLog["h2"]["IP"] = temp[0]["UEIP"]
-                BSLog["h2"]["POWER"] = temp[0]["UEPOWER"]
-                BSLog["h2"]["PRICE"] = temp[0]["UEPRICE"]
-                BSLog["h2"]["LOSS"] = temp[0]["UELOSS"]
-                BSLog["h2"]["MAX"] = temp[0]["UEMAX"]
-            elif temp[0]["UEIP"] == "10.0.0.3" and BSLog["h3"]["flag"] == False:
-                BSLog["h3"]["flag"] = True
-                BSLog["h3"]["IP"] = temp[0]["UEIP"]
-                BSLog["h3"]["POWER"] = temp[0]["UEPOWER"]
-                BSLog["h3"]["PRICE"] = temp[0]["UEPRICE"]
-                BSLog["h3"]["LOSS"] = temp[0]["UELOSS"]
-                BSLog["h3"]["MAX"] = temp[0]["UEMAX"]
-            lenth-=1
-            if BSLog["h1"]["flag"] == True and BSLog["h2"]["flag"] == True and BSLog["h3"]["flag"]==True:
-                break
+    # # the BS start to create the UE's info
+    # BSLog={
+    #     "h1":{},
+    #     "h2":{},
+    #     "h3":{}
+    #     # "h4":{},
+    #     # "h5":{},
+    #     # "h6":{}
+    # }
+    # BSLog["h1"]["flag"]=False
+    # BSLog["h2"]["flag"]=False
+    # BSLog["h3"]["flag"]=False
+    # filename1 = "/home/shlled/mininet-wifi/Log/BSLog.json"
+    # with open(filename1,'r') as f1:
+    #     buffer=f1.readlines()
+    #     lenth=len(buffer)
+    #     print(lenth)
+    #     while lenth>0:
+    #         temp=buffer[lenth-1]
 
-    "BS use UE info to decide which UE send which packet"
+    #         temp=json.loads(temp)
+    #         print(temp,"\n")
+    #         if temp[0]["UEIP"] == "10.0.0.1" and BSLog["h1"]["flag"] == False:
+    #             BSLog["h1"]["flag"] = True
+    #             BSLog["h1"]["IP"] = temp[0]["UEIP"]
+    #             BSLog["h1"]["POWER"] = temp[0]["UEPOWER"]
+    #             BSLog["h1"]["PRICE"] = temp[0]["UEPRICE"]
+    #             BSLog["h1"]["LOSS"] = temp[0]["UELOSS"]
+    #             BSLog["h1"]["MAX"] = temp[0]["UEMAX"]
+    #         elif temp[0]["UEIP"] == "10.0.0.2" and BSLog["h2"]["flag"] == False:
+    #             BSLog["h2"]["flag"] = True
+    #             BSLog["h2"]["IP"] = temp[0]["UEIP"]
+    #             BSLog["h2"]["POWER"] = temp[0]["UEPOWER"]
+    #             BSLog["h2"]["PRICE"] = temp[0]["UEPRICE"]
+    #             BSLog["h2"]["LOSS"] = temp[0]["UELOSS"]
+    #             BSLog["h2"]["MAX"] = temp[0]["UEMAX"]
+    #         elif temp[0]["UEIP"] == "10.0.0.3" and BSLog["h3"]["flag"] == False:
+    #             BSLog["h3"]["flag"] = True
+    #             BSLog["h3"]["IP"] = temp[0]["UEIP"]
+    #             BSLog["h3"]["POWER"] = temp[0]["UEPOWER"]
+    #             BSLog["h3"]["PRICE"] = temp[0]["UEPRICE"]
+    #             BSLog["h3"]["LOSS"] = temp[0]["UELOSS"]
+    #             BSLog["h3"]["MAX"] = temp[0]["UEMAX"]
+    #         lenth-=1
+    #         if BSLog["h1"]["flag"] == True and BSLog["h2"]["flag"] == True and BSLog["h3"]["flag"]==True:
+    #            break
 
+    # "BS use UE info to decide which UE send which packet"
 
-
-    # info("*** Start sending information\n")
+    info("*** Start sending information\n")
+    "BS should not control the behavior of UE,what UE has send isn't full"
+    thread_list = []
+    t1 = MyThread(command, args=(h2, "python receive.py 10.0.0.2 h2-eth0"))
+    # t1 = threading.Thread(target=command,args=(h2,"python receive.py 10.0.0.2 h2-eth0"))
+    thread_list.append(t1)
+    t2 = MyThread(command, args=(h1, "python send.py  10.0.0.1 h1-eth0 10.0.0.2 0.30 msg.txt"))
+    # t2 = threading.Thread(target=command,args=(h1,"python send.py  10.0.0.1 h1-eth0 10.0.0.2 0.15 msg.txt"))
+    thread_list.append(t2)
+    t1.start()
+    t2.start()
+    for t in thread_list:
+        t.join()
+    miss_pkt = thread_list[0].get_result()
+    info("*** Start sending the miss pkg\n")
+    #filename4 = '/home/shlled/mininet-wifi/Log/miss.txt'
+    filename4 = '/media/psf/Home/Documents/GitHub/mininet-project/Stackelberg/Log/miss.txt'
+    with open(filename4, 'r+') as f4:
+        buffer = f4.readlines()
+        lenth = len(buffer)
+        miss_pkt = buffer[lenth - 1]
+    print(miss_pkt)
+    t3 = threading.Thread(target=command, args=(h2, "python receive.py 10.0.0.2 h2-eth0"))
+    thread_list.append(t3)
+    t4 = threading.Thread(target=command,
+                          args=(h1, "python send.py 10.0.0.1 h1-eth0 10.0.0.2 0 msg.txt False '%s'" % miss_pkt))
+    thread_list.append(t4)
+    t3.start()
+    t4.start()
+    for t in thread_list:
+        t.join()
     # index=0
     # num = 0 #packge number
     # "find the size of the data"
-    # #filename2 = "/home/shlled/mininet-wifi/Log/msg.txt"
-    # filename2 = "/media/psf/Home/Documents/GitHub/mininet-wifi-project/Stackelberg/Log/msg.txt"
+    # filename2 = "/home/shlled/mininet-wifi/Log/msg.txt"
     # f2 = open(filename2,'r')
     # buffer2= f2.readlines()
     # lenth = len(buffer2)
@@ -146,13 +195,12 @@ def topology():
     #     while flag:
     #         try:
     #             thread.start_new_thread(command,(h2,"python receive.py 10.0.0.2 h2-eth0 0.15"))
-    #             thread.start_new_thread(command,(h1,"python send.py 10.0.0.1 h1-eth0 10.0.0.2 %d" % num))
+    #             thread.start_new_thread(command,(h1,"python send.py 10.0.0.1 h1-eth0 10.0.0.2 msg.txt %d" % num))
     #         except:
     #             print("send error")
     #         time.sleep(18) # wait thread finish
     #         print("send finish")
-    #         #filename3 = "/home/shlled/mininet-wifi/Log/final.txt"
-    #         filename3 = "/media/psf/Home/Documents/GitHub/mininet-wifi-project/Stackelberg/Log/final.txt"
+    #         filename3 = "/home/shlled/mininet-wifi/Log/final.txt"
     #         f3 = open(filename3,'r')
     #         buffer3 = f3.readlines()
     #         temp = len(buffer3)
@@ -160,14 +208,12 @@ def topology():
     #         if temp == index + 10 or temp >= lenth:
     #             flag = False
     #     index += 10
-         #num += 1
+    #     num += 1
     info("*** Running CLI\n")
     CLI_wifi(net)
 
     # h2.cmd("python receive.py 10.0.0.2 h2-eth0")
     # h1.cmd("python send.py 10.0.0.1 h1-eth0 10.0.0.2")
-
-
 
     info("*** Stopping network\n")
     net.stop()
--- Stackelberg/send.py
+++ Stackelberg/send.py
@@ -8,41 +8,80 @@ import time
 import sys
 import fire
 import json
-
+import random
+def stringToList(s):
+    if s == '':
+        return []
+    s = s[1:len(s)-1]
+    s = s.replace(' ', '')
+    print(s)
+    return [int(i) for i in s.split(',')]
 ''' 
     --num  the number of the packet
     --pow  the power of sending msg
     --times the max time of sending
+    --flag  if flag = false ,that means sending the missing packet using miss_pkt params
 '''
-def send(src, iface, dst, num=0, pow=5,times=10,send_pkt=[]):
-    t = 0
-    index = 0+num*10
-    filename1='/media/psf/Home/Documents/GitHub/mininet-project/Stackelberg/Log/msg.txt'
-    f1=open(filename1,'r')
-    buffer=f1.readlines()
-    lenth=len(buffer)
-    while index<lenth and t<times:
-        time.sleep(1)
-        now = time.time()
-        alpha=buffer[index]
-        msg = "send_time: " + "%.6f" % float(now) + " msg: " + alpha
-        send_pkt.append(msg)
-        print(msg)
-        p = Ether() / IP(src=src, dst=dst) / ICMP() / msg
-        sendp(p, iface = iface)
-        t += 1
-        index+=1
-        # alpha = chr(ord(alpha) + 1)
-    f1.close()
-    filename2='/Users/fanc/Documents/GitHub/mininet-project/Stackelberg/Log/UE%s.json' % src[7:9]
-    #update the pow after sending msg
-    with open(filename2,'r+') as f2:
-        buffer = f2.readlines()
-        lenth = len(buffer)
-        #data =buffer[0]
-        data = json.loads(buffer[lenth-1])
-        data["POWER"]-= pow 
-        json.dump(data,f2)
-        f2.write("\n")
+def send(src, iface, dst, loss, filename,flag = True,miss_pkt='',pow=5, times=10,send_pkt=[]):
+    if flag:
+        index = 0
+        #filename1='/home/shlled/mininet-wifi/Log/%s' % filename
+        filename1 = '/media/psf/Home/Documents/GitHub/mininet-project/Stackelberg/Log/%s' % filename
+        f1=open(filename1,'r')
+        buffer=f1.readlines()
+        lenth=len(buffer)
+        total=lenth
+        while index<lenth:
+            time.sleep(0.5)
+            now = time.time()
+            alpha=buffer[index]
+            #alpha=buffer[index].strip()
+            msg = "send_time: " + "%.6f" % float(now) + " filename:%s" % filename  + "total:%d" % total + "index:%d" % index + "data:" + alpha
+            send_pkt.append(msg)
+            print(msg)
+            p = Ether() / IP(src=src, dst=dst) / ICMP() / msg
+
+            "miss packet"
+            top=int(100-100*loss)
+            key=random.randint(1,100)
+            if key in range(1,top):
+                sendp(p, iface = iface)
+            else:
+                print("can't send the packet\n")
+
+            index+=1
+        f1.close()
+    else:
+        #filename1='/home/shlled/mininet-wifi/Log/%s' % filename
+        filename1 = '/media/psf/Home/Documents/GitHub/mininet-project/Stackelberg/Log/%s' % filename
+        f1=open(filename1,'r')
+        buffer=f1.readlines()
+        lenth=len(buffer)
+        total=lenth
+        #miss_pkt = stringToList(miss_pkt)
+        print("miss_pkt in send.py:",miss_pkt)
+        while miss_pkt!=[]:
+            print(miss_pkt)
+            time.sleep(0.1)
+            now = time.time()
+            alpha=buffer[int(miss_pkt[0])]
+            print('alpha', alpha)
+            msg = "send_time: " + "%.6f" % float(now) + " filename:%s " % filename  + "total:%d " % total + "index:%d " % miss_pkt[0] + "data:" + alpha
+            send_pkt.append(msg)
+            print(msg)
+            p = Ether() / IP(src=src, dst=dst) / ICMP() / msg
+            sendp(p, iface = iface)
+            miss_pkt.pop(0)
+        f1.close()
+    # filename2='/home/shlled/mininet-wifi/Log/UE%s.json' % src[7:9]
+    # #update the pow after sending msg
+    # with open(filename2,'r+') as f2:
+        # buffer = f2.readlines()
+        # lenth = len(buffer)
+        # #data =buffer[0]
+        # data = json.loads(buffer[lenth-1])
+        # data["POWER"]-= pow
+        # json.dump(data,f2)
+        # f2.write("\n")
 
 fire.Fire(send)
